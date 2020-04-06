e5d1c222ab68c181c3093fbc874729544d45d6a5
Ramses <ramses@cuatromedios.com>
2020-04-06 14:10:17 -0500

    Basic setup of css with preprocessor workflow using laravel mix

--- backend/resources/views/html_layout.blade.php
+++ backend/resources/views/html_layout.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>@yield(('title'))</title>
+    <meta charset="UTF-8">
+    <title>@yield(('title'))</title>
+    <link rel="stylesheet" type="text/css" href="/styles/main.css">
 </head>
 <body>
 <header>
