8aeed28670c6929c4eb68bfd1482709b39359048
Ramses <ramses@cuatromedios.com>
2020-04-12 14:21:34 -0500

    Casts moved to model EntityModel.php

--- server/app/Models/Entity.php
+++ server/app/Models/Entity.php
@@ -9,21 +9,5 @@ namespace App\Models;
  */
 class Entity extends EntityModel
 {
-    /**
-     * @var array A list of columns from the entities tables and other joins needs to be casted
-     */
-    protected $casts = [
-        'properties' => 'array',
-        'tags' => 'array',
-        'child_relation_tags' => 'array',
-        'descendant_relation_tags' => 'array',
-        'siblings_relation_tags' => 'array',
-        'media_tags' => 'array',
-        'relation_tags' => 'array',
-        'is_active' => 'boolean'
-    ];
 
-    public function getContentAttribute($contents) {
-        return "A";
-    }
 }
--- server/app/Models/EntityModel.php
+++ server/app/Models/EntityModel.php
@@ -30,6 +30,20 @@ class EntityModel extends Model
     protected $propertiesFields = [];
     private $storedContents = [];
 
+    /**
+     * @var array A list of columns from the entities tables and other joins needs to be casted
+     */
+    protected $casts = [
+        'properties' => 'array',
+        'tags' => 'array',
+        'child_relation_tags' => 'array',
+        'descendant_relation_tags' => 'array',
+        'siblings_relation_tags' => 'array',
+        'media_tags' => 'array',
+        'relation_tags' => 'array',
+        'is_active' => 'boolean'
+    ];
+
     /**********************
      * SCOPES
      **********************/
