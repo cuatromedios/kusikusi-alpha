8a5285633a30bbc8ce94b4f78fdfa6ffa9613273
Ramses Moreno <ramses@cuatromedios.com>
2020-04-17 13:05:57 -0500

    Updated api documentator to latest version that allows to define an output folder

--- server/config/apidoc.php
+++ server/config/apidoc.php
@@ -12,7 +12,7 @@ return [
     /*
      * Static output folder: Uncomment and change it if you need the documentation to be generated in a different folder
      */
-    // 'output_folder' => 'public/docs',
+    'output_folder' => '../docs/api',
 
     /*
      * Settings for `laravel` type output.
