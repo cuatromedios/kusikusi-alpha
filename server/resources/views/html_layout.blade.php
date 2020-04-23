f7043106d7452ac47129617d95836490cf89423c
Ramses Moreno <ramses@cuatromedios.com>
2020-04-23 12:21:51 -0500

    Set body classes based on entity only if the entity really exists

--- server/resources/views/html_layout.blade.php
+++ server/resources/views/html_layout.blade.php
@@ -5,7 +5,7 @@
     <title>@yield(('title'))</title>
     <link rel="stylesheet" type="text/css" href="/styles/main.css">
 </head>
-<body class="model-{{$entity->model ?? 'undefined'}} view-{{$entity->view ?? 'undefined'}} id-{{$entity->id}}">
+<body class="{{isset($entity) ? 'model-'.$entity->model.' view-'.$entity->view.' id-'.$entity->id : ''}}">
 <header>
 
 </header>
